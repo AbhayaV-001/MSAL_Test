a2437645f6dd813f87c8432e5bc4cef25f2e2125
Abhaya V <abhaya_v@lilly.com>
2024-02-07 10:21:27 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,37 @@
+// client/src/App.js
+import React, { useState, useEffect } from 'react';
 
+function App() {
+  const [message, setMessage] = useState('');
+
+  const fetchMessage = async () => {
+    try {
+      const response = await fetch(`${window.location.origin}/api/message`, {
+        mode: 'cors',
+        method: 'GET',
+        credentials: 'include', // include credentials for cross-origin requests with cookies
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch message from backend');
+      }
+
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      console.error('Error fetching message:', error.message);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>message from backend below</h1>
+        <h2>{message}</h2>
+        <button onClick={fetchMessage}>Fetch Message</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
